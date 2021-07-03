f27af8a9ecdc554d50b2da39c17611dbf8837a32
Sebastien Estienne <sebastien.estienne@gmail.com>
2021-07-03 10:48:27 -0700

    Cleaning the code and improving the logging and retry logic

--- wave_plus_exporter.py
+++ wave_plus_exporter.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
 
+import time
 import click
 import asyncio
 import struct
 import logging
 from bleak import BleakScanner, BleakClient
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict, Tuple
 from aioprometheus import Gauge, Service, Registry
 
 WP_UUID = "b42e1c08-ade7-11e4-89d3-123b93f75cba"
 WP_SENSOR_UUID = "b42e2a68-ade7-11e4-89d3-123b93f75cba"
 WP_MFDATA_SN_IDX = 820
 
+DEFAULT_HTTP_ADDR = "127.0.0.1"
+DEFAULT_HTTP_PORT = 9745
+# We try to collect metrics every 4 minutes, WavePlus only update
+# the metrics every 5 minutes but we want to account for possible
+# connection errors that will trigger retries
+DEFAULT_UPDATE_METRICS_FREQUENCY = 4 * 60
 
 logger = logging.getLogger("wave_plus_exporter")
-logger.setLevel(logging.DEBUG)
-logging.basicConfig(
-    format="%(asctime)s %(levelname)s: %(message)s",
-    # level=logging.DEBUG
-)
+logger.setLevel(logging.INFO)
+steam_handler = logging.StreamHandler()
+steam_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s: %(message)s"))
+logger.addHandler(steam_handler)
 
 
 class WavePlusDevice(object):
@@ -28,7 +34,7 @@ class WavePlusDevice(object):
         self.address = None
         self.detected = None
 
-    def set_address(self, address: str):
+    def set_address(self, address: str) -> None:
         self.address = address
         self.detected = self.address != None
 
@@ -41,7 +47,9 @@ class WavePlusDevice(object):
             f"detected={self.detected})"
         )
 
-    async def collect_metrics(self) -> None:
+    async def collect_metrics(
+        self, max_tries: int = 12, sleep_between_retries: int = 5
+    ) -> Optional[Tuple[int]]:
         if not self.detected:
             logger.error(
                 "%s (sn=%s) was not detected, can't collect metrics",
@@ -55,23 +63,44 @@ class WavePlusDevice(object):
             self.name,
             self.serial_number,
         )
-        async with BleakClient(self.address) as client:
-            raw_data = await client.read_gatt_char(WP_SENSOR_UUID)
-            raw_metrics = struct.unpack("BBBBHHHHHHHH", raw_data)
 
-            logger.info(
-                "collected metrics from %s (sn=%s): %s",
+        start_time = time.time()
+        raw_metrics = None
+        exc = None
+        for retry in range(max_tries):
+            try:
+                async with BleakClient(self.address) as client:
+                    raw_data = await client.read_gatt_char(WP_SENSOR_UUID)
+                    raw_metrics = struct.unpack("BBBBHHHHHHHH", raw_data)
+                    break
+            except Exception as exc:
+                await asyncio.sleep(sleep_between_retries)
+        collect_duration = int(time.time() - start_time)
+
+        if raw_metrics:
+            logger.debug(
+                "collected metrics from %s (sn=%s) in %ds with %d retries: %s",
                 self.name,
                 self.serial_number,
+                collect_duration,
+                retry,
                 raw_metrics,
             )
-            return raw_metrics
+        else:
+            logger.error(
+                "failed to collect metrics from %s (sn=%s) after %ds: %s",
+                self.name,
+                self.serial_number,
+                collect_duration,
+                exc,
+            )
+        return raw_metrics
 
 
 class WavePlusDeviceDetector(object):
-    def __init__(self, timeout: int = 5) -> None:
+    def __init__(self, scan_duration: int = 10) -> None:
         self.sn_map: Dict[int, str] = {}
-        self.timeout = timeout
+        self.scan_duration = scan_duration
         self.scanned = False
 
     @staticmethod
@@ -85,9 +114,9 @@ class WavePlusDeviceDetector(object):
         except IndexError:
             return None
 
-    async def scan(self):
+    async def scan(self) -> None:
         logger.info("scanning for AirThings Wave Plus devices...")
-        devices = await BleakScanner.discover(timeout=self.timeout)
+        devices = await BleakScanner.discover(timeout=self.scan_duration)
         for device in devices:
             uuids = device.metadata.get("uuids", [])
             if WP_UUID in uuids:
@@ -116,33 +145,43 @@ class WavePlusDeviceDetector(object):
 
 
 class WavePlusSensors(object):
+
+    HUMID = "humidity_percent"
+    RADON_S = "radon_short_term_avg_becquerels"
+    RADON_L = "radon_long_term_avg_becquerels"
+    TEMP = "temperature_celsius"
+    PRESS = "pressure_pascal"
+    CO2 = "carbondioxide_ppm"
+    VOC = "voc_ppb"
+
     def __init__(self) -> None:
         self.registry = Registry()
-        self.counters = [
-            Gauge("humidity_percent", r"Humidity (%rH)"),
-            Gauge("radon_short_term_avg_becquerels", "Radon short term Bq/m3"),
-            Gauge("radon_long_term_avg_becquerels", "Radon long term Bq/m3"),
-            Gauge("temperature_celsius", "Temperature (degC)"),
-            Gauge("pressure_pascal", "Pressure (hPa)"),
-            Gauge("carbondioxide_ppm", "CO2 (ppm)"),
-            Gauge("voc_ppb", "VOC (ppb)"),
-        ]
-        for counter in self.counters:
-            self.registry.register(counter)
+        counters = {
+            self.HUMID: r"Humidity (%rH)",
+            self.RADON_S: "Radon short term average (Bq/m3)",
+            self.RADON_L: "Radon long term average (Bq/m3)",
+            self.TEMP: "Temperature (°C)",
+            self.PRESS: "Relative atmospheric pressure (hPa)",
+            self.CO2: "CO2 Level (ppm)",
+            self.VOC: "TVOC level (ppb)",
+        }
+        for name, desc in counters.items():
+            self.registry.register(Gauge(name, desc))
 
-    def update_counters(self, name, raw_metrics):
+    def update_counters(self, name, raw_metrics) -> None:
         if raw_metrics[0] != 1:
             logger.error("invalid metrics: version=%s is not equal 1", raw_metrics[0])
             return
-        self.counters[0].set({"name": name}, raw_metrics[1] / 2.0)
-        self.counters[1].set({"name": name}, self.conv2radon(raw_metrics[4]))
-        self.counters[2].set({"name": name}, self.conv2radon(raw_metrics[5]))
-        self.counters[3].set({"name": name}, raw_metrics[6] / 100.0)
-        self.counters[4].set({"name": name}, raw_metrics[7] / 50.0)
-        self.counters[5].set({"name": name}, raw_metrics[8] * 1.0)
-        self.counters[6].set({"name": name}, raw_metrics[9] * 1.0)
+        self.registry.get(self.HUMID).set({"name": name}, raw_metrics[1] / 2.0)
+        self.registry.get(self.RADON_S).set({"name": name}, self._radon(raw_metrics[4]))
+        self.registry.get(self.RADON_L).set({"name": name}, self._radon(raw_metrics[5]))
+        self.registry.get(self.TEMP).set({"name": name}, raw_metrics[6] / 100.0)
+        self.registry.get(self.PRESS).set({"name": name}, raw_metrics[7] / 50.0)
+        self.registry.get(self.CO2).set({"name": name}, raw_metrics[8] * 1.0)
+        self.registry.get(self.VOC).set({"name": name}, raw_metrics[9] * 1.0)
 
-    def conv2radon(self, radon):
+    @staticmethod
+    def _radon(radon: int) -> int:
         if 0 <= radon <= 16383:
             return radon
         # Either invalid measurement, or not available
@@ -153,9 +192,9 @@ class WavePlusExporter(object):
     def __init__(
         self,
         devices: List[WavePlusDevice],
-        http_addr: str = "127.0.0.1",
-        http_port: int = 9745,
-        update_metrics_frequency: int = 60,
+        http_addr: str = DEFAULT_HTTP_ADDR,
+        http_port: int = DEFAULT_HTTP_PORT,
+        update_metrics_frequency: int = DEFAULT_UPDATE_METRICS_FREQUENCY,
     ) -> None:
         self.devices = devices
         self.http_addr = http_addr
@@ -167,11 +206,9 @@ class WavePlusExporter(object):
     async def update_metrics(self) -> None:
         while True:
             for device in self.devices:
-                try:
-                    raw_metrics = await device.collect_metrics()
+                raw_metrics = await device.collect_metrics()
+                if raw_metrics:
                     self.sensors.update_counters(device.name, raw_metrics)
-                except Exception as exc:
-                    logger.error("failed to update metrics: %s", exc)
             await asyncio.sleep(self.update_metrics_frequency)
 
     async def start(self) -> None:
@@ -188,11 +225,31 @@ class WavePlusExporter(object):
 
 
 @click.command()
-@click.option("--name", default="")
-@click.option("--http_addr", default="127.0.0.1")
-@click.option("--http_port", default=9745)
+@click.option(
+    "--debug",
+    is_flag=True,
+    help="Enable debug logging",
+)
+@click.option(
+    "--name",
+    default="",
+    help="Name or location of the device, if not provided the serial number is used",
+)
+@click.option(
+    "--http_addr", default=DEFAULT_HTTP_ADDR, help="IP for the HTTP server to listen on"
+)
+@click.option(
+    "--http_port",
+    default=DEFAULT_HTTP_PORT,
+    help="Port for the HTTP server to listen on",
+)
 @click.argument("serial_number", type=int)
-def main(name, http_addr, http_port, serial_number):
+def main(
+    debug: bool, name: str, http_addr: str, http_port: int, serial_number: int
+) -> None:
+    if debug:
+        logger.setLevel(logging.DEBUG)
+
     devices = [
         WavePlusDevice(serial_number, name),
     ]
